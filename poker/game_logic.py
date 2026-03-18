8bca8c6af9b49d59fb3956f4ea579bc793ad0e89
Benjamin Rodriguez <benjamin.rodriguez@iteso.mx>
2026-03-18 12:00:40 -0600

    fix: pre commit error

--- poker/game_logic.py
+++ poker/game_logic.py
@@ -296,7 +296,7 @@ class PokerGame:
         if not dealer_qualifies(dealer_eval):
             self.chips += self.ante + self.ante
             self.last_result = GameResult.DEALER_NO_QUALIFY
-            hand_name = dealer_eval[0].name.replace('_', ' ').title()
+            hand_name = dealer_eval[0].name.replace("_", " ").title()
             self.result_message = (
                 f"Dealer doesn't qualify ({hand_name}). "
                 f"Ante pays 1:1, bet is a push!"
